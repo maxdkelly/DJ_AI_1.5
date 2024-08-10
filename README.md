f2f2363c3251dbe6110f5c3dc2d45fc3b6890def
Max  Kelly <max.diamond.kelly@gmail.com>
2024-08-10 15:41:13 +1000

    more skeleton code

--- README.md
+++ README.md
@@ -1,3 +1,8 @@
 # DJ_AI_1.5
 DJ_AI but better
 https://soundcloud.com/user434808290/dj_ai-15
+
+```
+// setup virtual environment 
+python3 setup.py
+```
---
+++ dj_ai_1_5/orcheastor.py
@@ -0,0 +1,6 @@
+from pydub import AudioSegment
+from pydub.playback import play
+
+sound = AudioSegment.from_file("bin/track/MDX22.mp3", format="mp3")
+play(sound)
+
